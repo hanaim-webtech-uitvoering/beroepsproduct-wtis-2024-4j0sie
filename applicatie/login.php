f6a6e9cbcf45fd83bda9f04581a7b340948923e2
4j0sie <josephine120490@gmail.com>
2025-06-23 21:47:23 +0200

    alle registratie in zelfde bestand gedaan, login is nu functioneel en de css styling voor de foutmelding is aangemaakt

--- applicatie/login.php
+++ applicatie/login.php
@@ -1,4 +1,44 @@
-<?php require_once 'functies/data_functies.php'; ?>
+<?php require_once 'functies/db_connectie.php';
+require_once 'functies/data_functies.php';
+
+$foutmelding = '';
+
+if (isset($_SESSION['user'])) {
+    header('Location: index.php');
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = trim($_POST['username']);
+    $wachtwoord = $_POST['password'];
+
+    // Error melding moet ff mooier
+    if (empty($username) || empty($wachtwoord)) {
+        $foutmelding = "Vul alle verplichte velden in.";
+        exit;
+    }
+
+    $db = maakVerbinding();
+    $user = getUserInfo($username);
+
+    if (!$user || !password_verify($wachtwoord, $user['password'])) {
+        $foutmelding = "Ongeldige gebruikersnaam of wachtwoord.";
+    } else {
+        $_SESSION['user'] = [
+            'username' => $user['username'],
+            'voornaam' => $user['first_name'],
+            'achternaam' => $user['last_name'],
+            'adres' => $user['address'],
+            'rol' => $user['role'],
+            'postcode' => $user['postcode'],
+            'stad' => $user['city']
+        ];
+
+        header('Location: index.php');
+        exit;
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,13 +58,18 @@
     <?php include 'header.php'; ?>
     <main>
         <h1>Inloggen</h1>
-        <form method="POST" action="account.php">
+        <form method="POST" action="login.php">
             <label for="username">Gebruikersnaam *</label><br>
             <input type="text" id="username" name="username" placeholder="Voer uw gebruikersnaam in" required><br><br>
 
             <label for="password">Wachtwoord *</label><br>
             <input type="password" id="password" name="password" placeholder="Voer uw wachtwoord in" required><br><br>
 
+            <?php if (!empty($foutmelding)) { ?>
+                <div class="error-message">
+                    <?php echo htmlspecialchars($foutmelding); ?>
+                </div>
+            <?php } ?>
             <input type="submit" value="Inloggen"><br><br>
 
         </form>
