4a39ced0ae7d2075c493b12b8ef5ec26efcaf794
4j0sie <josephine120490@gmail.com>
2025-06-24 21:23:54 +0200

    Voeg nieuwe afbeeldingen toe voor menu-items en verbeter de indexpagina om het menu dynamisch weer te geven

--- applicatie/functies/data_functies.php
+++ applicatie/functies/data_functies.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'functies/db_connectie.php';
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
--- applicatie/index.php
+++ applicatie/index.php
@@ -1,4 +1,8 @@
-<?php require_once 'functies/data_functies.php'; ?>
+<?php require_once 'functies/data_functies.php';
+
+$menu = getMenu();
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,63 +22,28 @@
     <?php include 'header.php'; ?>
 
     <main>
-        <h1>Pizza's</h1>
-        <div class="box-container">
-            <div class="box">
-                <img src="images/margherita.png" alt="Pizza Margherita" class="box-image">
-                <p>Pizza Margherita</p>
-                <p>€5.99</p>
-                <button class="voeg-toe">+</button>
+        <?php foreach ($menu as $categorie => $producten) { ?>
+            <h1><?php echo htmlspecialchars($categorie); ?></h1>
+            <div class="box-container">
+                <?php foreach ($producten as $productNaam => $productInfo) { ?>
+                    <div class="box">
+                        <img src="images/<?php echo $productNaam; ?>.png" alt="<?php echo htmlspecialchars($productNaam); ?>"
+                            class="box-image">
+                        <p style="font-weight:bold;"><?php echo htmlspecialchars($productNaam); ?></p>
+                        <p class="ingredienten">Ingrediënten:
+                            <?php if (empty(implode(', ', $productInfo['ingredients']))) {
+                                echo 'Geen ingrediënten beschikbaar';
+                            } else {
+                                echo htmlspecialchars(implode(', ', $productInfo['ingredients']));
+                            } ?>
+                        </p>
+                        <p>€<?php echo htmlspecialchars($productInfo['price']); ?></p>
+                        <button class="voeg-toe">+</button>
+                    </div>
+                <?php } ?>
             </div>
-            <div class="box">
-                <img src="images/pepperoni.png" alt="Pepperoni Pizza" class="box-image">
-                <p>Pepperoni Pizza</p>
-                <p class="ingredienten">Ingrediënten: Tomaat, Mozzarella, Basilicum</p>
-                <p>€11.99</p>
-                <button class="voeg-toe">+</button>
-            </div>
-            <div class="box">
-                <img src="images/hawaii.png" alt="Pizza Hawaii" class="box-image">
-                <p>Pizza Hawaii</p>
-                <p>€9.99</p>
-                <button class="voeg-toe">+</button>
-            </div>
-            <div class="box">
-                <img src="images/salami.png" alt="Pizza Salami" class="box-image">
-                <p>Pizza Salami</p>
-                <p>€8.99</p>
-                <button class="voeg-toe">+</button>
-            </div>
-            <div class="box">
-                <img src="images/funghi.png" alt="Pizza Funghi" class="box-image">
-                <p>Pizza Funghi</p>
-                <p>€10.99</p>
-                <button class="voeg-toe">+</button>
-            </div>
-        </div>
+        <?php } ?>
 
-        <h1>Dranken</h1>
-        <div class="box-container">
-            <div class="box">
-                <img src="images/cola.png" alt="Coca-Cola" class="box-image">
-                <p>Coca-Cola</p>
-                <p>€2.99</p>
-                <button class="voeg-toe">+</button>
-            </div>
-            <div class="box">
-                <img src="images/fanta.png" alt="Fanta" class="box-image">
-                <p>Fanta</p>
-                <p>€2.99</p>
-                <button class="voeg-toe">+</button>
-            </div>
-            <div class="box">
-                <img src="images/sprite.png" alt="Sprite" class="box-image">
-                <p>Sprite</p>
-                <p>€2.99</p>
-                <button class="voeg-toe">+</button>
-            </div>
-
-        </div>
     </main>
     <footer>
         <p>© 2024 Pizzeria Sole Machina. Alle rechten voorbehouden.</p>
